7001ce42fa1621be2f68f3144f0bc57e9dd17f63
David Espinosa <david.emanuel.espinosa@gmail.com>
2019-03-28 07:49:46 -0300

    Add dynamoDB table and seed

--- handler.js
+++ handler.js
@@ -1,12 +1,28 @@
 'use strict';
 
+var AWS = require('aws-sdk');
+var dynamoDb = new AWS.DynamoDB.DocumentClient({
+  region: 'localhost',
+  endpoint: 'http://localhost:8000',
+  accessKeyId: 'DEFAULT_ACCESS_KEY',  // needed if you don't have aws credentials at all in env
+  secretAccessKey: 'DEFAULT_SECRET' // needed if you don't have aws credentials at all in env
+})
+
 module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+  const params = {
+    TableName: 'authorsTable'
   };
 
+  try {
+    const data = await dynamoDb.scan(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (error) {
+    return {
+      statusCode: error.statusCode,
+      body: JSON.stringify(error)
+    };
+  }
 };
