423ec78b27805b6e13859752cc65aed23785979f
David Espinosa <david.emanuel.espinosa@gmail.com>
2019-03-28 23:03:52 -0300

    Add seed data and config of tables

--- BackEnd/handler.js
+++ BackEnd/handler.js
@@ -3,7 +3,7 @@
 var AWS = require('aws-sdk');
 var dynamoDb = new AWS.DynamoDB.DocumentClient({
   region: 'localhost',
-  endpoint: 'http://localhost:8000',
+  endpoint: 'http://localhost:8001',
   accessKeyId: 'DEFAULT_ACCESS_KEY',  // needed if you don't have aws credentials at all in env
   secretAccessKey: 'DEFAULT_SECRET' // needed if you don't have aws credentials at all in env
 })
